9fb4eccf303d8b510f8b8d3b37db0b8a67167228
winniethebear424 <venniyu@gmail.com>
2023-12-16 21:19:04 -0800

    created templates with bootstrop for post outlay

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy  #handle redirection to homepage
+from .forms import PostForm  #allow users to upload the images via a form
+from .models import Post
 
 # Create your views here.
+
+class HomePageView(ListView):
+    model = Post
+    template_name = 'home.html'
+    
+class CreatePostView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'post.html'
+    success_url = reverse_lazy('home')
